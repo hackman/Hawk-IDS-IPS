4329a81f975945edc7ac475be5efd255a23e8a3c
vaLentin chernoZemski <valentin@siteground.com>
2010-03-11 10:51:22 +0200

    hawk.pl - Removed all SG related features + code indentation         Version changed to 3.0.0 	Removed all post_a_note calls         Removed ssh allowed_ips SG feature and all its functions 	Removed the pop/imap before smtp feature which was adding authenticated users in /etc/relayhosts for usage with exim 	Removed the IO errors and Memory errors notification systems 	Removed the SG SSH protection system a.k.a firewall_update user in the SSH bruteforce detector 	All timers used by the above functions has been removed

--- hawk.pl
+++ hawk.pl
@@ -7,28 +7,22 @@ use DBD::mysql;
 use POSIX qw(setsid), qw(strftime), qw(WNOHANG);
 
 require "/usr/local/sbin/parse_config.pm";
-require "/usr/local/sbin/post_a_note.pm";
 
 import parse_config;
-import post_a_note;
 
 # system variables
 $ENV{PATH} = '';		# remove unsecure path
-my $version = '2.00';	# version string
+my $VERSION = '3.0.0';	# version string
 
 # defining fault hashes
-my %ssh_faults = ();		# ssh faults storage
-my %ftp_faults = ();		# ftp faults storage
-my %pop3_faults = ();		# pop3 faults storage
-my %imap_faults = ();		# imap faults storage
-my %smtp_faults = ();		# smtp faults storage
-my %cpanel_faults = ();	# cpanel faults storage
-my %notifications = ();	# notifications
-my %possible_attackers = ();	# possible hack attempts
-
-my %authenticated_ips = ();	# authenticated_ips storage
-my %reported_ips = ();
-our %allowed_ips = ();
+my %ssh_faults = ();				# ssh faults storage
+my %ftp_faults = ();				# ftp faults storage
+my %pop3_faults = ();				# pop3 faults storage
+my %imap_faults = ();				# imap faults storage
+my %smtp_faults = ();				# smtp faults storage
+my %cpanel_faults = ();				# cpanel faults storage
+my %notifications = ();				# notifications
+my %possible_attackers = ();		# possible hack attempts
 
 my $conf = '/home/sentry/hackman/hawk-web.conf';
 my %config = parse_config($conf);
@@ -37,23 +31,15 @@ my %config = parse_config($conf);
 my $logfile = '/var/log/hawk.log';	# daemon logfile
 my $pidfile = '/var/run/hawk.pid';	# daemon pidfile
 my $log_list = '/usr/bin/tail -s 0.03 -F --max-unchanged-stats=20 /var/log/messages /var/log/secure /var/log/maillog /usr/local/cpanel/logs/access_log /usr/local/cpanel/logs/login_log |';
-my $authenticated_ips_file = '/etc/relayhosts';	# Authenticated to Dovecot IPs are stored here
 
-my $broot_time = 300;	# time(in seconds) before cleaning the hashes
-my $firewall_update_time = 5400; # time(in seconds) before updating the firewall
+my $broot_time = 300;				# time(in seconds) before cleaning the hashes
 my $clean_reported = 600;
-my $max_attempts = 5;	# max number of attempts(for $broot_time) before notify
-my $pop_max_time = 1800;
+my $max_attempts = 5;				# max number of attempts(for $broot_time) before notify
 
-my $debug = 0;			# by default debuging is OFF
-my $do_limit = 0;		# by default do not limit the offending IPs
-my $dovecot = 1;
+my $debug = 0;						# by default debuging is OFF
+my $do_limit = 0;					# by default do not limit the offending IPs
 my $start_time = time();
-my $io_last_notified = $start_time;
-my $io_already_notified = 0;
-my $mem_last_notified = $start_time;
-my $mem_already_notified = 0;
-my $pop_time = $start_time;
+
 my $fw_time = $start_time;
 my $myip = get_ip();
 my $hostname = '';
@@ -62,7 +48,7 @@ my %service_codes = split(/[:\s]/, $config{'service_ids'});
 # check for debug
 if (defined($ARGV[0])) {
 	if ($ARGV[0] =~ /debug/) {
-		$debug=1;		# turn on debuging
+		$debug=1;					# turn on debuging
 	}
 }
 
@@ -80,7 +66,7 @@ $0 = "[Hawk]";
 
 # open the logfile
 open HAWK, '>>', $logfile or die "DIE: Unable to open logfile $logfile: $!\n";
-logger("Hawk version $version started!");
+logger("Hawk version $VERSION started!");
 
 # check if the daemon is running
 if ( -e $pidfile ) {
@@ -137,59 +123,6 @@ sub cleanh {
 	logger("hashes cleaned!") if ($debug);
 }
 
-sub save_ip {
-	my $ip = shift;
-	if (!defined $authenticated_ips{$ip}) {
-		#logger("New pop3/imap authenticated IP $ip ... adding it to the list") if ($debug);
-		$authenticated_ips{$ip} = time();
-		open AUTH, '>>', $authenticated_ips_file;
-		print AUTH $ip, "\n";
-		close AUTH;
-	} else {
-		#logger("The hash for $ip already exists. Already added as popbeforesmtp ... skipping it!") if ($debug);
-	}
-}
-
-sub clean_ips {
-	my @to_be_removed = ();
-	# first get all IPs that are to be removed(duration more then $pop_max_time)
-	# and remove the IPs from authenticated_ips hash
-	while ( my ($k, $v) = each(%authenticated_ips) ) {
-		my $duration = time() - $v;
-		if ($duration >= $pop_max_time) {
-			logger("I will remove $k from the /etc/relayhosts file ... current time - $v > $pop_max_time") if ($debug);
-			push(@to_be_removed, $k);
-			delete($authenticated_ips{$k});
-		}
-	}
-	# now get the file into a string
-	if (open AUTH, '<', $authenticated_ips_file) {
-		my @auth_list = <AUTH>;
-		close AUTH;
-		chomp(@auth_list);
-		# write the new file without the ips from @to_be_removed
-		if (open AUTH, '>', $authenticated_ips_file) {
-			foreach my $auth_ip(@auth_list) {
-				my $skip_ip = 0;
-				chomp($auth_ip);
-				foreach my $ip (@to_be_removed) {
-					if ($auth_ip =~ /$ip/) {
-						logger("$auth_ip will be removed") if ($debug);
-						$skip_ip = 1;
-					}
-				}
-				next if ($skip_ip);
-				print AUTH $auth_ip, "\n";
-			}
-			close AUTH;
-		} else {
-			logger("I was unable to open $authenticated_ips_file for writing");
-		}
-	} else {
- 		logger("I was unable to open $authenticated_ips_file");
-	}
-}
-
 # check for broots
 sub check_broot {
 	while ( my ($k,$v) = each (%ssh_faults) ) {
@@ -210,19 +143,6 @@ sub check_broot {
 	while ( my ($k,$v) = each (%ftp_faults) ) {
 		notify('ftp', $k, $ftp_faults{$k}) if ( $v > $max_attempts );
 	}
-	#while ( my ($k,$v) = each (%possible_attackers) ) {
-	#	if ( $possible_attackers{$k}[0] > 10 ) {
-	#		if (defined($possible_attackers{$k}[3])) {
-	#			if ($possible_attackers{$k}[0] > 25 && $possible_attackers{$k}[3] < 3) {
-	#				$possible_attackers{$k}[3] = 6;
-	#				post_a_note(1, 2, "BRUTEFORCE||$possible_attackers{$k}[2]||$k||$possible_attackers{$k}[0]");
-	#			}
-	#		} else {
-	#			post_a_note(1, 2, "BRUTEFORCE||$possible_attackers{$k}[2]||$k||$possible_attackers{$k}[0]");
-	#			$possible_attackers{$k}[3] = 1;
-	#		}
-	#	}
-	#}
 }
 
 # Fork to background
@@ -252,32 +172,6 @@ open LOGS, $log_list or die "DIE: Unable to open logs: $!\n";
 select((select(HAWK), $| = 1)[0]);
 select((select(LOGS), $| = 1)[0]);
 
-sub firewall_update {
-	my %own_rules = ();
-	eval {
-		local $SIG{ALRM} = sub { die 'alarm'; };
-		alarm 5;
-		use IO::Socket::INET;
-		if ( my $sock = new IO::Socket::INET ( PeerAddr => 'master.sgvps.net', PeerPort => '80', Proto => 'tcp', Timeout => '3') ) {
-			# Send the faulty drive!
-			print $sock "GET /~sentry/cgi-bin/hawkup.cgi?server=$hostname\n\n";
-			while (<$sock>) {
-				chomp($_);
-				#logger("Socket answer $_") if ($debug);
-				$own_rules{$_} = $_;
-			}
-			close $sock;
-			while (my $ip = each (%own_rules)) {
-				#logger("Allowed IPs hash entry $ip") if ($debug); 
-			}
-		} else {
-			logger("Unable to get our allowed hosts list: $!");
-		}
-        alarm 0;
-    };
-	return %own_rules;
-}
-
 # Clean the zombie childs!
 sub sigChld {
 	while (waitpid(-1,WNOHANG)>0 ) {
@@ -293,7 +187,7 @@ sub store_to_db {
 	# $_[0] 0 for insert into failed_log || 1 for insert into broots a.k.a 0 for log_me || 1 for broot_me
 	# $_[1] IP
 	# $_[2] The service under attack - 0 = ftp, 1 = ssh, 2 = pop3, 3 = imap, 4 = webmail, 5 = cpanel
-	# $_[3] Is the user who is brooteforcing only if $_[0] == log_me
+	# $_[3] The user who is bruteforcing only if $_[0] == log_me
 	our $conn = DBI->connect_cached( $config{"db"}, $config{"dbuser"}, $config{"dbpass"}, { PrintError => 1, AutoCommit => 1 })
 		or logger("Unable to connect to the database while trying to log $_[0]: $!");
 	our $log_me = $conn->prepare('INSERT INTO failed_log ( ip, service, "user" ) VALUES ( ?, ?, ? ) ') or logger("Unable to prepare the log query: $!");
@@ -327,7 +221,7 @@ sub notify {
 		cPanel => '2083'
 	);
 	# log to DB and file
-	if ( ! exists $notifications {$_[1]} ) { 
+	if (! exists($notifications{$_[1]})) { 
 		$notifications{$_[1]}=1;
 		store_to_db(1, $_[1], $_[0]);
 		logger("!!! $_[0] $_[1] failed $_[2] times in $broot_time seconds") if ($debug);
@@ -349,21 +243,13 @@ sub notify {
 	}
 }
 
-%allowed_ips = firewall_update();
-
 while (<LOGS>) {
 	# Dovecot IMAP & POP3
 	if ($_ =~ /pop3-login:|imap-login:/) {
 		my @current_line = split /\s+/, $_;
 		my $current_service = 'imap';
 		$current_service = 'pop3' if ($current_line[5] =~ /pop3-login:/);
-
-		if ((defined $current_line[10] && $current_line[10] eq 'Login') || (defined $current_line[11] && $current_line[11] eq 'Login')) {
-			#If the customer is authenticated via normal Login or TLS/secured Login save the ip inside the relayhosts
-			next if ($current_line[8] !~ /rip/);
-			$current_line[8] =~ s/rip=(.*),/$1/;
-			save_ip($current_line[8]);
-		} elsif ($_ =~ /auth failed/) {
+		if ($_ =~ /auth failed/) {
 			my $user = $_;
 			my $ip = $_;
 			my $attempts = $_;
@@ -401,36 +287,13 @@ while (<LOGS>) {
 			}
 			logger("Service: $current_service IP: $ip User: $user Attempts: $attempts") if ($debug);
 		}
-	} elsif ( $_ =~ /I\/O error/i ) { 
-		# Feb 14 19:18:35 serv01 kernel: end_request: I/O error, dev sdb, sector 1405725148
-		my $io_time_now = time();
-		next if (($io_time_now - $io_last_notified) < 900 && $io_already_notified == 1);
-		my @line = split /\s+/, $_;
-		post_a_note(1, 1, "$line[9]||$_");
-		logger("IO error detected: $_") if ($debug);
-		$io_already_notified = 1 if ($io_already_notified  == 0); 
-		$io_last_notified = $io_time_now;
- 	} elsif ( $_ =~ /kernel: EDAC/) {
-		my $mem_time_now = time();
-		next if (($mem_time_now - $mem_last_notified) < 900 && $mem_already_notified == 1);
-		post_a_note(1, 9, "$_");
-		logger("Memory corruption detected: $_") if ($debug);
-		$mem_already_notified = 1 if ($mem_already_notified  == 0);
-		$mem_last_notified = $mem_time_now;
 	} elsif ( $_ =~ /sshd\[[0-9].+\]:/) {
 		my $ip = '';
 		my $user = '';
 		#sshd issue is bruteforce which will be stored to the db and notified to the monitoring if 1.
 		#if 2 we only send a notification.
-		my $ssh_issue = 0;
-		if ($_ =~ /Failed \w \w/ ||
-			$_ =~ /authentication failure/ ||
-			$_ =~ /Invalid user/i ||
-			#$_ =~ /Connection closed by/ ||
-			$_ =~ /Bad protocol/) {
-			# After if starts here :)
+		if ($_ =~ /Failed \w \w/ || $_ =~ /authentication failure/ || $_ =~ /Invalid user/i || $_ =~ /Bad protocol/) {
 			my @sshd = split /\s+/, $_;
-			$ssh_issue = 1;
 			if ( $sshd[8] =~ /invalid/ ) {
 				#May 16 03:27:24 serv01 sshd[25536]: Failed password for invalid user suport from ::ffff:85.14.6.2 port 52807 ssh2
 				#May 19 22:54:19 serv01 sshd[21552]: Failed none for invalid user supprot from 194.204.32.101 port 20943 ssh2
@@ -438,12 +301,6 @@ while (<LOGS>) {
 				$ip = $sshd[12];
 				$user = $sshd[10];
 				logger("sshd: Incorrect V1 $user $ip") if ($debug);
-			#} elsif ( $sshd[5] =~ /Connection/ ) {
-			#	#May 25 02:11:34 serv01 sshd[10146]: Connection closed by 87.118.135.130
-			#	$sshd[8] =~ s/::ffff://;
-			#	$ip = $sshd[8];
-			#	$user = 'none';
-			#	logger("sshd: Incorrect KEY $user $ip") if ($debug);
 			} elsif ( $sshd[5] =~ /Invalid/) {
 				#May 19 22:54:19 serv01 sshd[21552]: Invalid user supprot from 194.204.32.101
 				$sshd[9] =~ s/::ffff://;
@@ -473,57 +330,30 @@ while (<LOGS>) {
 				$user = $sshd[8];
 				logger("sshd: Incorrect V3 $user $ip") if ($debug);
 			}
-		} elsif ($_ =~ /Accepted publickey for/ || $_ =~ /Accepted password for/ || $_ =~ /Postponed publickey for/) {
-			logger("It seems that someone logged in? $_") if ($debug);
-			#May 25 00:48:58 serv01 sshd[16015]: Accepted publickey for root from 75.125.60.5 port 32863 ssh2
-			#May 20 00:51:54 serv01 sshd[20568]: Accepted password for support from ::ffff:67.15.245.5 port 50336 ssh2
-			#Jun  8 17:13:39 serv01 sshd[4474]: Postponed publickey for root from 77.70.33.151 port 35098 ssh2
-			my %protected_users = (
-				'root' => '0',
-				'support' => '0'
-			);
-			my @sshd = split /\s+/, $_;
-			$sshd[10] =~ s/::ffff://;
-			$ip = $sshd[10];
-			$user = $sshd[8];
-			my $mask = $ip;
-			$mask =~ s/\.[0-9]{1,3}$/\.0/;
-
-			logger("The mask is now $mask") if ($debug);
-
-			next if (!exists $protected_users{$user});
-			next if ((defined($allowed_ips{$ip}) || defined($allowed_ips{$mask})) && $protected_users{$user} == 0);
-			$ssh_issue = 2;
-			logger("sshd: Login $user $ip") if ($debug);
 		} else {
 			logger("Did not found any matches for $_") if ($debug);
 			next;
 		} 
 
 		$_ =~ s/\'//g;
-		if ($ssh_issue == 1) {
-			next if ( $ip =~ /$myip/ || $ip =~ /127.0.0.1/ );	# this is the local server
-			#post_a_note(1, 2, "BRUTEFORCE||$service_codes{'ssh'}||$ip||$_");
-			# $_[2] The service under attack - 0 = ftp, 1 = ssh, 2 = pop3, 3 = imap, 4 = webmail, 5 = cpanel
-			# Store the bastard to the database
-			store_to_db(0, $ip, 1, $user);
-			# Increase the attempts
-			if ( exists $ssh_faults {$ip} ) {
-				$ssh_faults{$ip}++;
-			} else {
-				$ssh_faults{$ip} = 1;
-			}
-			# Mark it as possible attacker for the notices system!
-			if (exists $possible_attackers{$ip}) {
-				$possible_attackers{$ip}[0]++;
-				$possible_attackers{$ip}[1] = $user;
-				logger("Possible attacker update: IP: $ip Attempts: $possible_attackers{$ip}[0] Line: $_") if ($debug);
-			} else {
-				$possible_attackers{$ip} = [ 1, $user, $service_codes{'ssh'} ];
-				logger("Possible attacker update: IP: $ip Attempts: $possible_attackers{$ip}[0] Line: $_") if ($debug);
-			}
-		} elsif ($ssh_issue == 2) {
-			post_a_note(1, 2, "UNAUTHORIZED||$service_codes{'ssh'}||$ip||$_");
+		next if ( $ip =~ /$myip/ || $ip =~ /127.0.0.1/ );	# this is the local server
+		# $_[2] The service under attack - 0 = ftp, 1 = ssh, 2 = pop3, 3 = imap, 4 = webmail, 5 = cpanel
+		# Store attacker's ip to the database
+		store_to_db(0, $ip, 1, $user);
+		# Increase the attempts
+		if ( exists $ssh_faults {$ip} ) {
+			$ssh_faults{$ip}++;
+		} else {
+			$ssh_faults{$ip} = 1;
+		}
+		# Mark it as possible attacker for the notices system!
+		if (exists $possible_attackers{$ip}) {
+			$possible_attackers{$ip}[0]++;
+			$possible_attackers{$ip}[1] = $user;
+			logger("Possible attacker update: IP: $ip Attempts: $possible_attackers{$ip}[0] Line: $_") if ($debug);
+		} else {
+			$possible_attackers{$ip} = [ 1, $user, $service_codes{'ssh'} ];
+			logger("Possible attacker update: IP: $ip Attempts: $possible_attackers{$ip}[0] Line: $_") if ($debug);
 		}
 	} elsif ( $_ =~ /pure-ftpd:/ && $_ =~ /Authentication failed/ ) {
 		# May 16 03:06:43 serv01 pure-ftpd: (?@85.14.6.2) [WARNING] Authentication failed for user [mamam]
@@ -571,18 +401,8 @@ while (<LOGS>) {
 	my $curr_time = time();
 
 	if (($curr_time - $start_time) > $broot_time) {		# if the passed time is grater then $broot_time
-		cleanh();							# clean the hashes
-		$start_time = time();				# set the start_time to now
-	}
-
-	if (($curr_time - $pop_time) > 300) {
-		clean_ips();				# clean the authenticated_ips_file
-		$pop_time = time();			# set the pop_time to now
-	}
-
-	if (($curr_time - $fw_time) > $firewall_update_time) {
-		%allowed_ips = firewall_update();
-		$fw_time = time();
+		cleanh();										# clean the hashes
+		$start_time = time();							# set the start_time to now
 	}
 }
 
