fe76962b819f9a1903d6389e1de878bc3683e074
Deyan Doychev <deyandoichev@gmail.com>
2010-03-23 14:17:31 +0200

    Adding form validation. Also added a config hash to get the form's values from.

--- web/js/hawk.js
+++ web/js/hawk.js
@@ -1,14 +1,57 @@
-
-var fieldValidator = function () {
-	if (this.getValue() > 0) {
-		return true;
-	}
-	return false;
+config = {
+	min_brutes_i: 11,
+	max_brutes_i: 11,
+	min_failed_i: 11,
+	max_failed_i: 11,
+	min_blocked_i: 11,
+	max_blocked_i: 11,
 }
+
+var validated = false;
+
+Ext.apply(Ext.form.VTypes, {
+	minMaxNumber: function(value, field) {
+		if (value == '' || value == null) {
+			this.minMaxNumberText = 'This field must not be left blank.';
+			return false;
+		}
+		value = Number(value);
+		if ( value < 0) {
+			this.minMaxNumberText = 'This field must contain a positive integer.';
+			return false;
+		}
+		var name = field.getName();
+		var other;
+		if (/^min/.test(name)) {
+			name = name.replace('min', 'max');
+			other = Ext.getCmp(name);
+			if (other.getValue() < value) {
+				this.minMaxNumberText = 'This field must contain a smaller value than the corresponding max field.';
+				return false;
+			}
+		} else {
+			name = name.replace('max', 'min');
+			other = Ext.getCmp(name);
+			if (other.getValue() > value) {
+				this.minMaxNumberText = 'This field must contain a larger value than the corresponding min field.';
+				return false;
+			}
+		}
+		if (!validated) {
+			validated = true;
+			Ext.getCmp(name).validate();
+		} else {
+			validated = false;
+		}
+		return true;
+	},
+	minMaxNumberText: 'The field must contain a positive number, greater than or equal to the corresponding min value or less than or equal to the correspondong max value.',
+});
 
 var data2 = [{hour:'08:00', brutes:298, failed:2252, blocked:1050},{hour:'09:00', brutes:213, failed:2194, blocked:1061},{hour:'10:00', brutes:3496, failed:2361, blocked:1134},{hour:'11:00', brutes:3460, failed:2640, blocked:1267},{hour:'12:00', brutes:2946, failed:2289, blocked:1140},{hour:'13:00', brutes:3650, failed:2751, blocked:1324},{hour:'14:00', brutes:3239, failed:2394, blocked:1253},{hour:'15:00', brutes:3200, failed:2360, blocked:1155},{hour:'16:00', brutes:3636, failed:2755, blocked:1324},{hour:'17:00', brutes:4012, failed:352, blocked:1729},{hour:'18:00', brutes:3505, failed:243, blocked:1391},{hour:'19:00', brutes:3301, failed:2567, blocked:1354},{hour:'20:00', brutes:3277, failed:2385, blocked:11252},{hour:'21:00', brutes:3860, failed:3027, blocked:1565},{hour:'22:00', brutes:2945, failed:2554, blocked:1268},{hour:'23:00', brutes:3369, failed:2798, blocked:1320},{hour:'00:00', brutes:3404, failed:2340, blocked:1161},{hour:'01:00', brutes:3312, failed:2533, blocked:1306},{hour:'02:00', brutes:3084, failed:2409, blocked:1221},{hour:'03:00', brutes:3069, failed:2447, blocked:1255},{hour:'04:00', brutes:3629, failed:2719, blocked:1330},{hour:'05:00', brutes:3113, failed:2535, blocked:1293},{hour:'06:00', brutes:3658, failed:2743, blocked:1522},{hour:'07:00', brutes:3133, failed:2309, blocked:1353}];
 
 Ext.onReady(function () {
+	Ext.QuickTips.init();
 	stores = new Array();
 
 		setTimeout('stores[0].loadData(data2, false);', 5000);
@@ -77,9 +120,8 @@ Ext.onReady(function () {
 				labelStyle: 'width:150px',
 				fieldLabel: 'Min bruteforce attempts',
 				allowBlank: false,
-				value: 11,//min_bruteforce,
-				blankText:'Please enter a bruteforce attempts value',
-				validator: fieldValidator,
+				value: config['min_brutes_i'],//min_bruteforce,
+				vtype:'minMaxNumber',
 			}),
 			new Ext.form.NumberField({
 				width: 40,
@@ -87,9 +129,8 @@ Ext.onReady(function () {
 				labelStyle: 'width:150px',
 				fieldLabel: 'Max bruteforce attempts',
 				allowBlank: false,
-				value: 11,//max_bruteforce,
-				blankText:'Please enter a bruteforce attempts value',
-				validator: fieldValidator,
+				value: config['max_brutes_i'],//max_bruteforce,
+				vtype:'minMaxNumber',
 			}),
 			new Ext.form.NumberField({
 				width: 40,
@@ -97,9 +138,8 @@ Ext.onReady(function () {
 				labelStyle: 'width:150px',
 				fieldLabel:'Min failed attempts',
 				allowBlank: false,
-				value: 11,//min_failed,
-				blankText:'Please enter a failed attempts value',
-				validator: fieldValidator,
+				value: config['min_failed_i'],//min_failed,
+				vtype:'minMaxNumber',
 			}),
 			new Ext.form.NumberField({
 				width: 40,
@@ -107,9 +147,8 @@ Ext.onReady(function () {
 				labelStyle: 'width:150px',
 				fieldLabel:'Max failed attempts',
 				allowBlank: false,
-				value: 11,//max_failed,
-				blankText:'Please enter a failed attempts value',
-				validator: fieldValidator,
+				value: config['max_failed_i'],//max_failed,
+				vtype:'minMaxNumber',
 			}),
 			new Ext.form.NumberField({
 				width: 40,
@@ -117,9 +156,8 @@ Ext.onReady(function () {
 				labelStyle: 'width:150px',
 				fieldLabel:'Min blocked IP addresses',
 				allowBlank:false,
-				emptyText: 11,//min_blocked,
-				validator: fieldValidator,
-				blankText:'Please enter a blocked IP addresses count'
+				value: config['min_blocked_i'],//min_blocked,
+				vtype:'minMaxNumber',
 			}),
 			new Ext.form.NumberField({
 				width: 40,
@@ -127,9 +165,8 @@ Ext.onReady(function () {
 				labelStyle: 'width:150px',
 				fieldLabel:'Max blocked IP addresses',
 				allowBlank:false,
-				emptyText: 11,//max_blocked,
-				validator: fieldValidator,
-				blankText:'Please enter a blocked IP addresses count'
+				value: config['max_blocked_i'],//max_blocked,
+				vtype:'minMaxNumber',
 			}),
 			],
 		buttons: [{
@@ -148,6 +185,7 @@ Ext.onReady(function () {
 			},{
 			text: "Close",
 			handler: function() {
+					// clear the data
 					mySettings.hide();
 				}
 			}]
