abe8e2219f4dfd2a7f1acc00915430ab3f18ba58
Jivko Angelov <jivko@jivko-angelovs-macbook-pro.local>
2010-03-29 18:32:35 +0300

    Work on the new hawk local interface: 	- Finished loading the data from api for: 		a) bruteforce attempts chart; 		b) failed attempts chart; 		c) summary IP addresses count; 		d) summary report (1 hour, 24 hours, weekly)

--- web-local/js/hawk.js
+++ web-local/js/hawk.js
@@ -1,12 +1,11 @@
 Ext.chart.Chart.CHART_URL = 'ext-3.1.1/resources/charts.swf';
+var base_url = '/~sentry/cgi-bin/jivko/';
 
 config = {
 	chartLineSize: 1,
 	chartDotSize: 5,
 }
 
-var charts = new Array();
-var summary_grid = new Array();
 var ResultsWin;
 
 function ShowResults(ipaddr) {
@@ -118,100 +117,54 @@ function ShowResults(ipaddr) {
 }
 
 Ext.onReady(function(){
-	var serv_data = [
-		[20, 5, 13, 8, 21, 56]
-	];
-	var summary = [
-		['125.65.7.3', '28'],
-		['24.42.33.11', '4'],
-		['24.99.104.193', '67'],
-		['58.181.108.157', '32'],
-		['67.235.226.74', '3'],
-		['70.26.185.43', '44'],
-		['71.118.16.242', '12'],
-		['72.55.137.101', '3'],
-		['74.55.182.98', '6'],
-		['74.162.17.73', '81']
-	];
-	var charts_store = new Ext.data.JsonStore({
-		fields:['hour', 'brutes', 'failed', 'blocked'],
-		data: [
-			{hour:'08:00', brutes:298, failed:2252, blocked:1050},
-			{hour:'09:00', brutes:213, failed:2194, blocked:1061},
-			{hour:'10:00', brutes:3496, failed:2361, blocked:1134},
-			{hour:'11:00', brutes:3460, failed:2640, blocked:1267},
-			{hour:'12:00', brutes:2946, failed:2289, blocked:1140},
-			//{hour:'13:00', brutes:3650, failed:2751, blocked:1324},
-			//{hour:'14:00', brutes:3239, failed:2394, blocked:1253},
-			//{hour:'15:00', brutes:3200, failed:2360, blocked:1155},
-			{hour:'16:00', brutes:0, failed:0, blocked:0},
-			//{hour:'17:00', brutes:4012, failed:352, blocked:1729},
-			//{hour:'18:00', brutes:3505, failed:243, blocked:1391},
-			//{hour:'19:00', brutes:3301, failed:2567, blocked:1354},
-			{hour:'20:00', brutes:3277, failed:2385, blocked:11252},
-			{hour:'21:00', brutes:3860, failed:3027, blocked:1565},
-			{hour:'22:00', brutes:2945, failed:2554, blocked:1268},
-			{hour:'23:00', brutes:3369, failed:2798, blocked:1320},
-			{hour:'00:00', brutes:3404, failed:2340, blocked:1161},
-			{hour:'01:00', brutes:3312, failed:2533, blocked:1306},
-			{hour:'02:00', brutes:3084, failed:2409, blocked:1221},
-			//{hour:'03:00', brutes:3069, failed:2447, blocked:1255},
-			//{hour:'04:00', brutes:3629, failed:2719, blocked:1330},
-			{hour:'05:00', brutes:3113, failed:2535, blocked:1293},
-			{hour:'06:00', brutes:3658, failed:2743, blocked:1522},
-			{hour:'07:00', brutes:3133, failed:2309, blocked:1353}
+	var charts = new Array();
+	var charts_store;
+	var chartsObj = new Object({
+		options: [
+			{title: '<a href="hawk-brute.html">Bruteforce attempts statistics</a>', type: 'broots', name: 'bruteforce attempts', color: '0xff0000'},
+			{title: '<a href="hawk-failed.html">Failed login attempts</a>', type: 'failed_log', name: 'failed attempts', color: '0x0000ff'}
 		]
 	});
 	
-	charts[0] = new Ext.Panel({
-		title: '<a href="hawk-brute.html">Bruteforce attempts statistics</a>',
-		width:440,
-		height:220,
-		style: {
-			float: 'left',
-			'margin': 5,
-		},
-		items: {
-			xtype: 'linechart',
-			store: charts_store,
-			xField: 'hour',
-			yField: 'brutes',
-			series: [{
-				type: 'line',
-				displayName: 'bruteforce attempts',
+	for (var j=0; j<chartsObj.options.length; j++) {
+		charts_store = new Ext.data.JsonStore({
+			autoLoad: true,
+			url: '../cgi-bin/jivko/hawk.pl',
+			baseParams: {
+				id: '3',
+				type: chartsObj.options[j].type
+			},
+			fields: [
+				{name: 'count', mapping: 0, type: 'int'},
+				{name: 'hour', mapping: 1}
+			]
+		});
+		charts.push(new Ext.Panel({
+				title: chartsObj.options[j].title,
+				width:440,
+				height:220,
 				style: {
-					color:0xff0000,
-					size: config.chartDotSize,
-					lineSize: config.chartLineSize,
+					float: 'left',
+					'margin': 5,
+				},
+				items: {
+					xtype: 'linechart',
+					store: charts_store,
+					xField: 'hour',
+					yField: 'count',
+					series: [{
+						type: 'line',
+						displayName: chartsObj.options[j].name,
+						style: {
+							color: chartsObj.options[j].color,
+							size: config.chartDotSize,
+							lineSize: config.chartLineSize,
+						}
+					}]
 				}
-			}]
-		}
-	});
-	
-	charts[1] = new Ext.Panel({
-		title: '<a href="hawk-failed.html">Failed login attempts</a>',
-		width:440,
-		height:220,
-		style: {
-			float: 'left',
-			'margin': 5,
-		},
-		items: {
-			xtype: 'linechart',
-			store: charts_store,
-			xField: 'hour',
-			yField: 'failed',
-			series: [{
-				type: 'line',
-				displayName: 'failed attempts',
-				style: {
-					color:0x0000ff,
-					size: config.chartDotSize,
-					lineSize: config.chartLineSize,
-				}
-			}]
-		}
-	});
+			})
+		);
+	}
 	
 	var chartsPanel = new Ext.Panel({
 		id:'charts-panel',
@@ -260,18 +213,22 @@ Ext.onReady(function(){
 		})
 	});
 	
-	var brute_store = new Ext.data.ArrayStore({
+	var brute_store = new Ext.data.JsonStore({
+		autoLoad: true,
+		url: '../cgi-bin/jivko/hawk.pl',
+		baseParams: {
+			id: 1
+		},
+		idProperty: 'ftp',
 		fields: [
-			{name: 'FTP'},
-			{name: 'SSH'},
-			{name: 'POP3'},
-			{name: 'IMAP'},
-			{name: 'WebMail'},
-			{name: 'cPanel'}
+			{name: 'FTP', mapping: "ftp"},
+			{name: 'SSH', mapping: "ssh"},
+			{name: 'POP3', mapping: "pop3"},
+			{name: 'IMAP', mapping: "imap"},
+			{name: 'WebMail', mapping: "webmail"},
+			{name: 'cPanel', mapping: "cpanel"}
 		]
 	});
-
-	brute_store.loadData(serv_data);
 
 	var brute_grid = new Ext.grid.GridPanel({
 		store: brute_store,
@@ -296,62 +253,45 @@ Ext.onReady(function(){
 		title: 'Bruteforce attempts statistics per service',      
 	});
 	
-	var summary_store = new Ext.data.ArrayStore({
-		fields: [
-			{name: 'IP address'},
-			{name: 'Count'}
+	var summary_store;
+	var summary_grid = new Array();
+	summaryObj = new Object({
+		options: [
+			{interval: '1 hours', title: '1 hour summary'},
+			{interval: '24 hours', title: 'Daily summary'},
+			{interval: '1 weeks', title: 'Weekly summary'},
 		]
 	});
-
-	summary_store.loadData(summary);
-
-	summary_grid[0] = new Ext.grid.GridPanel({
-		store: summary_store,
-		columns: [
-			{header: 'IP address', sortable: true, width: 142},
-			{header: 'Count', sortable: true, width: 142}
-		],
-		width: 290,
-		height: 265,
-		//layout: 'fit',
-		style: {
-			float: 'left',
-			'margin': 5
-		},
-		title: '1 Hour summary',      
-	});
-
-	summary_grid[1] = new Ext.grid.GridPanel({
-		store: summary_store,
-		columns: [
-			{header: 'IP address', sortable: true, width: 142},
-			{header: 'Count', sortable: true, width: 142}
-		],
-		width: 290,
-		height: 265,
-		//layout: 'fit',
-		style: {
-			float: 'left',
-			'margin': 5
-		},
-		title: 'Daily summary',      
-	});
-
-	summary_grid[2] = new Ext.grid.GridPanel({
-		store: summary_store,
-		columns: [
-			{header: 'IP address', sortable: true, width: 142},
-			{header: 'Count', sortable: true, width: 142}
-		],
-		width: 290,
-		height: 265,
-		//layout: 'fit',
-		style: {
-			float: 'left',
-			'margin': 5
-		},
-		title: 'Weekly summary',      
-	});
+	for (var i=0; i<summaryObj.options.length; i++) {
+		summary_store = new Ext.data.JsonStore({
+			autoLoad: true,
+			url: '../cgi-bin/jivko/hawk.pl',
+			baseParams: {
+				id: '2',
+				interval: summaryObj.options[i].interval
+			},
+			fields: [
+				{name: 'count', mapping: 0},
+				{name: 'ip', mapping: 1}
+			]
+		});
+	
+		summary_grid.push(new Ext.grid.GridPanel({
+				store: summary_store,
+				columns: [
+					{header: 'IP address', sortable: true, width: 142, dataIndex: 'ip'},
+					{header: 'Count', sortable: true, width: 142, dataIndex: 'count'}
+				],
+				width: 290,
+				height: 265,
+				style: {
+					float: 'left',
+					'margin': 5
+				},
+				title: summaryObj.options[i].title,      
+			})
+		);
+	}
 
 	var summaryPanel = new Ext.Panel({
 		id:'summary-panel',
