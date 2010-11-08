a3c42f33dc432d7302011a9f4cf1f04ba40ba9a0
vaLentin chernoZemski <valentin@siteground.com>
2010-11-08 14:54:10 +0200

    hawk-unblock.sh - Make sure to catch the block expiration time from the hawk conf directly.

--- bin/hawk-unblock.sh
+++ bin/hawk-unblock.sh
@@ -4,10 +4,11 @@ hawk_conf='/home/1h/etc/hawk.conf'
 dbuser=$(awk -F = '/dbuser/{print $2}' $hawk_conf)
 dbpass=$(awk -F = '/dbpass/{print $2}' $hawk_conf)
 block_list=$(awk -F = '/block_list/{print $2}' $hawk_conf)
+block_expire=$(awk -F = '/block_expire/{print $2}' $hawk_conf)
 
 export PGUSER="$dbuser"
 export PGPASSWD="$dbpass"
-psql -t -n -A -F " " hawk -c "SELECT ip,id FROM blacklist WHERE date_rem IS NULL AND date_add < (now() - interval '24 hours')" | while read ip id; do
+psql -t -n -A -F " " hawk -c "SELECT ip,id FROM blacklist WHERE date_rem IS NULL AND date_add < (now() - interval '$block_expire seconds')" | while read ip id; do
 	if [ "$ip" != '' ] && [ "$id" != '' ]; then
 		echo "`date` - Removing $ip($id) from DB and firewall"
 		psql hawk -c "UPDATE blacklist SET date_rem=now() WHERE id = '$id'"
